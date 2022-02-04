93eedb3ab4492a799b25726839283fd70bdb4631
OrenVildermanPepperi <oren.v@pepperi.com>
2022-02-05 01:41:31 +0200

    ignore for git and lint and first version of the app.ts and general.service

--- app.ts
+++ app.ts
@@ -1,25 +1,30 @@
-import express, { Application, Request, Response } from "express"
-import 'dotenv/config'
-import { ConsoleColors, GeneralService } from "./services/service.general";
-import indexRouter from "./routes";
+import express, { Application, Request, Response } from 'express';
+import 'dotenv/config';
+import { ConsoleColors/* , GeneralService */ } from './services/general.service';
+import indexRouter from './routes';
 
-const generalService = new GeneralService();
-const job = generalService.initiateNewUserCronJob(2);
-job.start();
+// const generalService = new GeneralService();
+// const job = generalService.initiateNewPeopleCronJob(2);;
+// job.start();
 
 const PORT: number = Number(process.env.PORT) || 5000;
-const app: Application = express()
+const app: Application = express();
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use('/', indexRouter)
+
+app.use('/', (request: Request, response: Response, next) => {
+  console.log(`%cReviced - ${request.method}:${request.url}`, ConsoleColors.IncomingRequest);
+  next();
+});
+app.use('/api/V0.1', indexRouter);
 
 app.listen(PORT, () => {
-    console.log(`%cServer is running on http://localhost:${PORT}`, ConsoleColors.SystemInformation)
+  console.log(`%cServer is running on http://localhost:${PORT}`, ConsoleColors.SystemInformation);
 });
 
 app.all('*', (request: Request, response: Response) => {
-    console.log(`%cPage not found for requset: ${request.url}`, ConsoleColors.Error);
-    response
-        .status(404).send('<h1>Page not found</h1>');
+  console.log(`%cPage not found for requset: ${request.url}`, ConsoleColors.Error);
+  response
+    .status(404).send('<h1>Page not found</h1>');
 });
---
+++ services/general.service.ts
@@ -0,0 +1,189 @@
+import 'dotenv/config';
+import * as fs from 'fs';
+import * as path from 'path';
+import WebSocket from 'ws';
+import { QueryAPIService } from './index';
+
+const fetch = require('node-fetch');
+const { CronJob } = require('cron');
+const createCsvStringifier = require('csv-writer').createObjectCsvStringifier;
+
+declare type exportFormat =
+  | 'csv'
+  | 'txt'
+  | 'json'
+
+/**
+ * ConsoleColors are used in this demo instead the use of logger types: log/debug/error
+ * This is to make the the project easier to display
+ */
+export const ConsoleColors = {
+  SystemInformation: 'color: #F87217',
+  Information: 'color: #FFD801',
+  AssertionStatusLog: 'color: #893BFF',
+  FetchStatus: 'color: #893BFF',
+  SqlQuery: 'color: #00FFFF',
+  Error: 'color: #FF0000',
+  Success: 'color: #00FF00',
+};
+
+export interface FetchStatusResponse {
+  Ok: boolean;
+  Status: number;
+  Headers?: any;
+  Body: any;
+  Error: any;
+}
+
+type HttpMethod = 'POST' | 'GET' | 'PUT' | 'DELETE' | 'PATCH';
+
+export interface FetchRequestInit {
+  method?: HttpMethod;
+  body?: string;
+  headers?: {
+    [key: string]: string;
+  };
+  timeout?: number;
+  size?: number;
+}
+
+export default class GeneralService {
+  fetchStatus(url: string, requestInit?: FetchRequestInit): Promise<FetchStatusResponse> {
+    const start = performance.now();
+    let responseStr: string;
+    let parsed: any = {};
+    let errorMessage: any = {};
+    return fetch(url, {
+      method: `${requestInit?.method ? requestInit?.method : 'GET'}`,
+      body: typeof requestInit?.body === 'string' ? requestInit.body : JSON.stringify(requestInit?.body),
+      headers: {
+        ...requestInit?.headers,
+      },
+      timeout: requestInit?.timeout,
+      size: requestInit?.size,
+    })
+      .then(async (response = {} as any) => {
+        const end = performance.now();
+        const isSucsess = !!(response.status > 199 && response.status < 400);
+        console[isSucsess ? 'log' : 'error'](
+          `%cFetch ${isSucsess ? '' : 'Error '}${requestInit?.method ? requestInit?.method : 'GET'}: ${url
+          } took ${(end - start).toFixed(2)} milliseconds`,
+          `${isSucsess ? ConsoleColors.FetchStatus : ConsoleColors.Information}`,
+        );
+        try {
+          if (response.headers.get('content-type')?.startsWith('image')) {
+            responseStr = await response.buffer().then((r = {} as any) => r.toString('base64'));
+            parsed = {
+              Type: 'image/base64',
+              Text: responseStr,
+            };
+          } else {
+            responseStr = await response.text();
+            parsed = responseStr ? JSON.parse(responseStr) : '';
+          }
+        } catch (error) {
+          if (responseStr && responseStr.substring(20).includes('xml')) {
+            parsed = {
+              Type: 'xml',
+              Text: responseStr,
+            };
+            errorMessage = error;
+          } else if (responseStr && responseStr.substring(20).includes('html')) {
+            parsed = {
+              Type: 'html',
+              Text: responseStr,
+            };
+            errorMessage = error;
+          } else {
+            parsed = {
+              Type: 'Error',
+              Text: responseStr,
+            };
+            errorMessage = error;
+          }
+        }
+
+        const headersArr: any = {};
+        response.headers.forEach((value: any, key: string | number) => {
+          headersArr[key] = value;
+        });
+
+        return {
+          Ok: response.ok,
+          Status: response.status,
+          Headers: headersArr,
+          Body: parsed,
+          Error: errorMessage,
+        };
+      })
+      .catch((error: { type: any; name: any; message: any; }) => {
+        console.error(`Error type: ${error.type}, ${error}`);
+        return {
+          Ok: undefined as any,
+          Status: undefined as any,
+          Headers: undefined as any,
+          Body: {
+            Type: error.type,
+            Name: error.name,
+          },
+          Error: error.message,
+        };
+      });
+  }
+
+  convertJsonToCsv = (json: any) => {
+    const headers = [];
+    for (const key in json[0]) {
+      headers.push({ id: key, title: key });
+    }
+
+    const csvStringifier = createCsvStringifier({ header: headers });
+
+    const headLine = csvStringifier.getHeaderString();
+    const dataRows = csvStringifier.stringifyRecords(json);
+    return `${headLine}${dataRows}`;
+  };
+
+  createTmpFileFromSqLiteTable = async (db: QueryAPIService, tableName: string, type: exportFormat) => {
+    const dbData = await db.getAllFromTable(tableName);
+    fs.writeFileSync(path.resolve(process.cwd(), `./db/tmp/out.${type}`), this.convertJsonToCsv(dbData));
+    return path.resolve(process.cwd(), `./db/tmp/out.${type}`);
+  };
+
+  /**
+   * This chron job will run every interval you set and add people from the random people generator API
+   * @param intervalTime
+   */
+  initiateNewPeopleCronJob(intervalTime: number) {
+    console.log(`%cCron Job Execution Set To Start Every: ${intervalTime} Minutes`, ConsoleColors.SystemInformation);
+    return new CronJob(`1/15 */${intervalTime} * * * *`, (async (): Promise<void> => {
+      console.log('%cCron Job Execution Started', ConsoleColors.SystemInformation);
+    }));
+  }
+
+  initiateDBTables() {
+    console.log('%cDB initiation started', ConsoleColors.SystemInformation);
+    throw new Error('NotImplementedException');
+  }
+
+  initiateWebSocket = () => {
+    const ws = new WebSocket(`ws://${process.env.WEBSOCKET || 'websocket-echo.com'}`);
+
+    ws.on('open', () => {
+      console.log('%cConnected To WebSocket', ConsoleColors.Information);
+    });
+
+    ws.on('close', () => {
+      console.log('%cDisconnected From WebSocket', ConsoleColors.Information);
+    });
+
+    /**
+     * Logger for the responses when using the default websocket: websocket-echo.com
+     */
+    ws.on('message', (data: string) => {
+      console.log(`%cThis message is eco from websocket-echo.com: ${data}`, ConsoleColors.Information);
+    });
+
+    return ws;
+  };
+}
