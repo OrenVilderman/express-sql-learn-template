fe281c72d7b223bf8d1f01d44c0f39b00fa4d48e
Oren Vilderman <OrenVilderman@gmail.com>
2022-02-20 02:21:53 +0200

    Refactor the APP to make it testable

--- app.ts
+++ app.ts
@@ -1,17 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 import 'dotenv/config';
-import { ConsoleColors, webSocket } from './services/general.service';
+import { ConsoleColors } from './services/general.service';
 import indexRouter from './routes';
 import { GeneralService, QueryAPIService } from './services';
 
 const generalService = new GeneralService();
-const job = generalService.initiateNewPeopleCronJob(2);
+const job = generalService.initiateNewPeopleCronJob(15);
 job.start();
 
 const queryAPIService = new QueryAPIService();
 queryAPIService.createDefultDBTablesIfMissing();
-
-const socketSet: any = new Set();
 
 const PORT: number = Number(process.env.PORT) || 5000;
 const app: Application = express();
@@ -26,45 +24,20 @@ app.use('/', (request: Request, response: Response, next) => {
 
 app.use('/api/V0.1', indexRouter);
 
-const server = app.listen(PORT, () => {
-  console.log(`%cServer is running on http://localhost:${PORT}`, ConsoleColors.SystemInformation);
-});
-
 app.all('*', (request: Request, response: Response) => {
   console.log(`%cPage not found for requset: ${request.url}, with method of: ${request.method}`, ConsoleColors.Error);
   response
     .status(404).send('<h1>Page not found</h1>');
 });
 
-process.on('SIGTERM', () => {
-  console.log('%cSIGTERM signal received: closing HTTP server', ConsoleColors.Error);
-
-  /**
-   * Close the connections before calling the server close
-   */
-  socketSet.forEach((element: { destroy: () => void; }) => {
-    element.destroy();
-  });
-  server.close();
+const server = app.listen(PORT, () => {
+  console.log(`%cServer is running on http://localhost:${PORT}`, ConsoleColors.SystemInformation);
 });
 
-/**
- * Store the connection to allow implemntaion of limitation
- */
-server.on('connection', (socket) => {
-  socketSet.add(socket);
-  socket.on('close', () => {
-    socketSet.delete(socket);
-  });
-});
+export default server;
 
-server.on('close', async () => {
-  console.log('%cServer Shut Down', ConsoleColors.Error);
-  job.stop();
-  webSocket.close();
-  /**
-   * Allow time to create logs in the server before closing
-   */
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  process.exit(1);
-});
+// Use for handlers
+export { job };
+
+// Add handlers
+require('./handlers.ts');
---
+++ handlers.ts
@@ -0,0 +1,40 @@
+import { ConsoleColors, webSocket } from './services/general.service';
+
+import server, { job } from './app';
+
+const socketSet: any = new Set();
+
+process.on('SIGTERM', () => {
+  console.log('%cSIGTERM signal received: closing HTTP server', ConsoleColors.Error);
+
+  /**
+   * Close the connections before calling the server close
+   */
+  socketSet.forEach((element: { destroy: () => void; }) => {
+    element.destroy();
+  });
+  server.close();
+});
+
+/**
+ * Store the connection to allow implemntaion of limitation
+ */
+server.on('connection', (socket) => {
+  socketSet.add(socket);
+  socket.on('close', () => {
+    socketSet.delete(socket);
+  });
+});
+
+server.on('close', async () => {
+  console.log('%cServer Shut Down', ConsoleColors.Error);
+  job.stop();
+  if (webSocket) {
+    webSocket.close();
+  }
+  /**
+   * Allow time to create logs in the server before closing
+   */
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  process.exit(1);
+});
